1908ba3a4dcb7d54ace33e2bb522bd27f637773e
ASTAR <apo2073@outlook.com>
2025-11-18 09:03:43 +0900

    Update site title and social links in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'GrassProject',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/grassproject' }],
 			sidebar: [
 				{
 					label: 'Guides',
@@ -23,4 +23,6 @@ export default defineConfig({
 			],
 		}),
 	],
+	site: 'https://grassproject.github.io',
+  	base: '/docs',
 });
